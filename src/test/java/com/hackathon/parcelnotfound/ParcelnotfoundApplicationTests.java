0ea56d4ef229b177554ac74986e28e0d7432691f
malkovskiyaa <malkovkiyaa@pochtabank.ru>
2019-09-14 01:22:45 +0300

    test

---
+++ src/test/java/com/hackathon/parcelnotfound/ParcelnotfoundApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackathon.parcelnotfound;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParcelnotfoundApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
